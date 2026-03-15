c4b687e5859da4a3864e71ef2425ff40b256856e
groomigroom <googjohn0143@gmail.com>
2026-03-15 17:16:03 +0900

    Update format specifier (서식 문자).c

--- format specifier (서식 문자).c
+++ format specifier (서식 문자).c
@@ -1,8 +1,9 @@
 #include <stdio.h>
-int main(){
+int main()
+{
   printf("내 나이 = %d 친구 나이 =%d", 29, 31);
   printf("민트초코 가격은 %f원입니다.", 3.3);
-  printf("구름이는 %c입니다.", 멍멍이);
+  printf("구름이는 %c입니다.", c);
 
   /* %d 정수
   %f 실수
