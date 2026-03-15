cea4c5f578d56056f31a12076e3da5e643be275f
groomigroom <googjohn0143@gmail.com>
2026-03-15 16:58:25 +0900

    Update and rename 근의 공식.c to quadratic formula(근의 공식).c

--- quadratic formula(근의 공식).c
+++ quadratic formula(근의 공식).c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <math.h>
+//<math.h>는 제곱근, 삼각함수, 지수, 로그 등 고급 수학 연산 함수들을 담고 있는 표준 라이브러리 헤더 파일입니다.
 
 int main(void)
 {
@@ -7,6 +8,7 @@ int main(void)
     double b = -2;
     double c = -3;
 
+//Square Root -> 제곱근, 루트
     double root1 = (-b + sqrt(b * b - 4 * a * c)) / (2 * a);
     double root2 = (-b - sqrt(b * b - 4 * a * c)) / (2 * a);
 
