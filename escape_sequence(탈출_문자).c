796d7ee487a8df152739ee72dd6647ce2e1522a6
groomigroom <googjohn0143@gmail.com>
2026-03-15 17:41:39 +0900

    Add escape_sequence.c to demonstrate escape sequences
    Demonstrate various escape sequences in C.

---
+++ escape_sequence(탈출_문자).c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+  
+  printf("\t12345");
+  printf("\b67890");
+  printf("\n12345");
+  printf("\r67890");
+
+/*
+\n 붙여서 줄바꿈 출력도
+\t로 탭 바꿈
+\b 한 칸 뒤
+\r 맨 앞
+*/
+
+}
