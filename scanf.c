f92023f0e78dc7bc3ecab07af0394972b24a1094
groomigroom <googjohn0143@gmail.com>
2026-03-16 17:42:30 +0900

    Update scanf.c

--- scanf.c
+++ scanf.c
@@ -3,11 +3,12 @@
 int main()
 {
 	int num;
+	float fnum;
 
-	printf("정수 입력:");
-	scanf("%d", &num);
+	printf("정수와 실수 입력:");
+	scanf("%d %f", &num, &fnum);
 
-	printf("결과: %d\n", num);
+	printf("결과: %d %f\n", num, fnum);
 	
 	return 0;
 }
