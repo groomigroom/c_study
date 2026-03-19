b7a498bc4065235dcb2bacf330a08b08492ad5cd
groomigroom <googjohn0143@gmail.com>
2026-03-19 20:09:54 +0900

    Create 정보처리기사 82쪽 포인터와 배열.c

---
+++ 정보처리기사 82쪽 포인터와 배열.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+	int a[5];
+
+	int i;
+	int *p;
+	for (i = 0; i < 5; i++)
+
+		a[i] = i + 10;
+	p = a;
+	for (i = 0; i < 5; i++)
+
+		printf("%d", *(p+i));
+
+}
