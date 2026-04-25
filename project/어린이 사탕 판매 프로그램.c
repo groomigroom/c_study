34499ab6b16dbfa1674feefd5f5558b67f5f6cc1
groomigroom <googjohn0143@gmail.com>
2026-04-25 20:59:26 +0900

    Update and rename edit_어린이 사탕.c to 어린이 사탕 판매 프로그램.c

--- project/어린이 사탕 판매 프로그램.c
+++ project/어린이 사탕 판매 프로그램.c
@@ -2,10 +2,10 @@
 
 int main()
 {
-    char sname[20];
+    char sname[50];
     int icandy;
     printf("구매하고 싶은 사탕의 이름과 사탕의 개수를 입력하세요.\n 치아 건강을 위해서 현재 사탕을 5개까지만 구매 가능합니다.\n 예시: 하얀사탕 5\n");
-    scanf("%s %d", sname, &icandy);
+    scanf("%s %d", &sname, &icandy);
     printf("%s을 말씀하셨습니다.\n", sname);
     if (1 <= icandy && icandy <= 5)
     {
